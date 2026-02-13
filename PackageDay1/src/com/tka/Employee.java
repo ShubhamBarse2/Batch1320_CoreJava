aeb4bd3c7ba96e6280a5215f3562a1927646d53a
ShubhamBarse2 <shubhambarse510@gmail.com>
2026-02-13 18:50:28 +0530

    Add files via upload

---
+++ PackageDay1/src/com/tka/Employee.java
@@ -0,0 +1,9 @@
+package com.tka;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		System.out.println("this is Employee Class ");
+	}
+	
+}
---
+++ PackageDay1/src/com/tka/Student.java
@@ -0,0 +1,9 @@
+package com.tka;
+
+public class Student {
+
+	public static void main(String[] args) {
+		System.out.println("this is Student Class ");
+	}
+
+}
