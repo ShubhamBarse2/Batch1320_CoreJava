6643daee01429081bba7e837ff72c59ab54c21cf
ShubhamBarse2 <shubhambarse510@gmail.com>
2026-02-24 18:51:51 +0530

    Add files via upload

---
+++ GlobalVariablePractise.java
@@ -0,0 +1,34 @@
+package com.aa;
+
+public class GlobalVariablePractise {
+
+	static byte b;
+	static int a;
+	static short c;
+	static long d;
+	static float e;
+	static double f;
+	static char g;
+	static boolean h;
+	static String name = "Ram";
+
+//	int a; // global variable
+
+	public static void main(String[] args) {
+
+		int b; // local variable must need to initiliazed the value
+
+		System.out.println(b);
+
+//		System.out.println(a);
+//		System.out.println(b);
+//		System.out.println(c);
+//		System.out.println(d);
+//		System.out.println(e);
+//		System.out.println(f);
+//		System.out.println(g);
+//		System.out.println(h);
+//		System.out.println(name);
+	}
+
+}
