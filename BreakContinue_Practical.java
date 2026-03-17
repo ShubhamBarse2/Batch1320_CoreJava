a8b84e797de096682576b297eeccb4bd8145d03c
ShubhamBarse2 <shubhambarse510@gmail.com>
2026-03-17 19:19:34 +0530

    Add files via upload

---
+++ BreakContinue_Practical.java
@@ -0,0 +1,48 @@
+package com.jbk;
+
+public class BreakContinue_Practical {
+
+	public static void main(String[] args) {
+
+//		for (int i = 1; i <= 10; i++) {
+//			if (i == 5) {
+//				break;  // it is used to stop the loop 
+//			}
+//			System.out.println(i);
+//		}
+
+//		for (int i = 1; i <= 10; i++) {
+//			if (i == 5) {
+//				continue; // it is used to skip one iteration 
+//			}
+//			System.out.println(i);
+//		}
+
+		// i want to print even no
+
+//		for (int i = 1; i <= 10; i++) {
+//			if (i % 2 != 0) {
+//				continue; // it is used to skip one iteration
+//
+//			}
+//			System.out.println(i);
+//
+//		}
+
+//		for (int i = 1; i <= 30; i++) {
+//			if (i % 3 == 0) {
+//				continue; // it is used to skip one iteration
+//			}
+//			System.out.println(i);
+//		}
+
+//		for (int i = 1; i <= 30; i++) {
+//			if (i % 3 == 0 || i % 5 == 0) {
+//				continue; // it is used to skip one iteration
+//			}
+//			System.out.println(i);
+//		}
+
+	}
+
+}
---
+++ StarPattern_Programs.java
@@ -0,0 +1,62 @@
+package com.jbk;
+
+public class StarPattern_Programs {
+
+	public static void main(String[] args) {
+
+//		for (int i = 1; i <= 5; i++) { // rows
+//			for (int j = 1; j <= 5; j++) { // column
+//				System.out.print(i + " ");
+//			}
+//			System.out.println();
+//		}
+
+//		*
+//		**
+//		***
+//		****
+//		*****
+
+//		for (int i = 1; i <= 5; i++) { // rows
+//			for (int j = 1; j <= i; j++) { // column
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+
+//		for (int i = 1; i <= 5; i++) { // rows
+//			for (int j = 5; j >= i; j--) { // column
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+
+//		for (int i = 1; i <= 5; i++) { // rows
+//
+//			for (int j = 1; j <= 5 - i; j++) { // space
+//				System.out.print(" ");
+//			}
+//
+//			for (int j = 1; j <= i; j++) { // Column
+//				System.out.print("*");
+//			}
+//
+//			System.out.println();
+//
+//		}
+	}
+
+//	for (int i = 1; i <= 5; i++) { // rows
+//
+//		for (int j = 1; j <= 5 - i; j++) { // space
+//			System.out.print(" ");
+//		}
+//
+//		for (int j = 1; j <= i; j++) { // Column
+//			System.out.print("* ");
+//		}
+//
+//		System.out.println();
+//	}
+
+}
