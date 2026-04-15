d486b677835c0d92a8bc2e958412fc2ae616f854
ShubhamBarse2 <shubhambarse510@gmail.com>
2026-04-15 19:19:19 +0530

    Add files via upload

---
+++ String_Code.java
@@ -0,0 +1,84 @@
+package com.tka;
+
+public class String_Code {
+
+	public static void main(String[] args) {
+//		String str1 = "Ram";
+//		String str2 = "Ram";
+//
+//		System.out.println(str1.hashCode());
+//		System.out.println(str2.hashCode());
+//
+//		System.out.println(str1 == str2);      // checked memory address 
+//		System.out.println(str1.equals(str2)); // checked content 
+
+//		String str1 = "Ram";
+//		String str2 = "ram";
+//
+//		System.out.println(str1.hashCode());
+//		System.out.println(str2.hashCode());
+//
+//		System.out.println(str1 == str2);      // checked memory address 
+//		System.out.println(str1.equals(str2)); // checked content 
+
+//		String str1 = new String("Rahul");
+//		String str2 = new String("ramu");
+//		
+//		System.out.println(str1.hashCode());
+//		System.out.println(str2.hashCode());
+//
+//		System.out.println(str1 == str2);
+//		System.out.println(str1.equals(str2)); 
+
+//		String str1 = new String("Rahul");
+//		String str2 = new String("Rahul");
+//		
+//		System.out.println(str1.hashCode());
+//		System.out.println(str2.hashCode());
+//		
+//		
+//		System.out.println(System.identityHashCode(str1));
+//		System.out.println(System.identityHashCode(str2));
+//		
+//
+//		System.out.println(str1 == str2); // checked memory address  false
+//		System.out.println(str1.equals(str2)); // checked content   true
+
+//		String str1 = new String("Rahul");
+//		String str2 ="Rahul";
+//		
+//		System.out.println(str1.hashCode());
+//		System.out.println(str2.hashCode());
+//
+//		System.out.println(str1 == str2);
+//		System.out.println(str1.equals(str2)); 
+
+//		String name = "Ram";
+////		String SurName = "Yadav";
+//
+//		String fname = name.concat("Yadav");
+//
+//		System.out.println(name == fname);
+
+//		System.out.println(fullName.hashCode());
+//		System.out.println(name == fullName);
+
+//		String city1 = "pune";
+//		String city2 = "pune";
+//		System.out.println(city1 == city2);
+//		System.out.println(city1.equals(city2));
+
+//		              0123 
+		String str = "java";
+
+//		System.out.println(str.charAt(2));
+//		System.out.println(str.length());
+//		System.out.println(str.toUpperCase());
+//		System.out.println(str.toLowerCase());
+//		System.out.println(str.endsWith("w"));
+//		System.out.println(str.trim());
+//		System.out.println(str.substring(1,3));
+
+	}
+
+}
