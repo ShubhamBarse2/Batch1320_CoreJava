2b0eef59347d468135b01d47cfbcaee92bdff0c1
ShubhamBarse2 <shubhambarse510@gmail.com>
2026-05-04 18:26:10 +0530

    Add files via upload

---
+++ SetRefCode/A.java
@@ -0,0 +1,49 @@
+package SetRefCode;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class A {
+
+	public static void main(String[] args) {
+
+//		Set<Integer> set = new HashSet<Integer>();
+//		set.add(123);
+//		set.add(123);
+//		set.add(345);
+//		set.add(8756);
+//		set.add(988);
+//		set.add(null);
+//		set.add(null);
+
+//		System.out.println(set);
+
+//		for (Integer i : set) {
+//			System.out.println(i);
+//		}
+
+//		TreeSet<Integer> set = new TreeSet<Integer>();
+//		set.add(123);
+//		set.add(1235673);
+//		set.add(345);
+//		set.add(8756);
+//		set.add(988);
+//		set.add(null); null cannnot accepted in treeset bcz it provides sorting 
+
+//		System.out.println(set);
+
+//		LinkedHashSet<Integer> set = new LinkedHashSet<Integer>();
+//		set.add(123);
+//		set.add(123);
+//		set.add(345);
+//		set.add(8756);
+//		set.add(988);
+//		set.add(null);
+//
+//		System.out.println(set);
+
+	}
+
+}
---
+++ SetRefCode/B.java
@@ -0,0 +1,64 @@
+package SetRefCode;
+
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Vector;
+
+public class B {
+
+	public static void main(String[] args) {
+//
+//		List list = new ArrayList();
+//		list.add(456);
+//		list.add("Ram");
+//		list.add(true);
+//		list.add(45336);
+//		list.add(45);
+//		list.add(456234567653l);
+//		list.add(45.45f);
+
+//		ListIterator itr = list.listIterator();
+//		while (itr.hasNext()) {
+//			System.err.println(itr.next());
+//		}
+
+//		Iterator itr = list.iterator();
+//		while (itr.hasNext()) {
+//			System.out.println(itr.next());
+//		}
+//
+//		Vector list2 = new Vector();
+//		list2.add(456);
+//		list2.add("Ram");
+//		list2.add(true);
+//		list2.add(45336);
+//		list2.add(45);
+//		list2.add(456234567653l);
+//		list2.add(45.45f);
+//
+//		Enumeration eee = list2.elements();
+//		while (eee.hasMoreElements()) {
+//			System.out.println(eee.nextElement());
+//		}
+
+//		HashSet list2 = new HashSet();
+//		list2.add(456);
+//		list2.add("Ram");
+//		list2.add(true);
+//		list2.add(45336);
+//		list2.add(45);
+//		list2.add(456234567653l);
+//		list2.add(45.45f);
+//
+//		Iterator itr = list2.iterator();
+//		while (itr.hasNext()) {
+//			System.err.println(itr.next());
+//		}
+
+	}
+
+}
