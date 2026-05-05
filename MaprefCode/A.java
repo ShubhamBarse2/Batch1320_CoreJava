e4ddfee0f13c96d26ebfd81ef550a878e23d6bc2
ShubhamBarse2 <shubhambarse510@gmail.com>
2026-05-05 18:39:39 +0530

    Add files via upload

---
+++ MaprefCode/A.java
@@ -0,0 +1,89 @@
+package MaprefCode;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
+public class A {
+
+	public static void main(String[] args) {
+
+//		HashMap map = new HashMap();
+//
+//		
+//		
+//		map.put(123, "Ram");
+//		map.put(123.45, "Shyam");
+//		map.put(123, 234567);
+//		map.put(123, true);
+//		map.put(123, "Ram");
+//
+//		System.out.println(map);
+
+//		HashMap<Integer, String> map = new HashMap<Integer, String>();
+//
+//		map.put(123, "Ram");
+//		map.put(124, "Shyam");
+//		map.put(125, "Om");
+//		map.put(null, null);
+//		map.put(345, null);
+//		map.put(12, null);
+//		map.put(128, "Shyam");
+//
+//		System.out.println(map);
+
+//		TreeMap<Integer, String> map = new TreeMap<Integer, String>();
+//
+//		// Null as key not accepted bcz it provides an Sorting Features 
+//		
+//		map.put(123, "Ram");
+//		map.put(124, "Shyam");
+//		map.put(125, "Om");
+//		map.put(null, null);
+//		map.put(345, null);
+//		map.put(12, null);
+//		map.put(128, "Shyam");
+//
+//		System.out.println(map);
+
+//		LinkedHashMap<Integer, String> map = new LinkedHashMap<Integer, String>();
+
+		// Null as key not accepted bcz it provides an Sorting Features
+
+//		map.put(123, "Ram");
+//		map.put(124, "Shyam");
+//		map.put(125, "Om");
+//		map.put(null, null);
+//		map.put(345, null);
+//		map.put(12, null);
+//		map.put(128, "Shyam");
+
+//		System.out.println(map.get(123));
+
+//		for (Entry<Integer, String> data : map.entrySet()) {
+//			System.out.println(data.getKey() + " -> " + data.getValue());
+//		}
+
+//		Hashtable<Integer, String> mapp = new Hashtable<Integer, String>();
+//
+////		it cannot accept null key or null value
+//
+//		mapp.put(123, "Don");
+//		mapp.put(124, "java");
+//		mapp.put(125, "python");
+//		mapp.put(123, ".net");
+//		mapp.put(123, "ee");
+//
+//		System.out.println(mapp);
+		
+//		Stack & Queue 
+		
+//		Java 8 features  :
+		
+//		Stream Api , Lambda Functions 
+
+	}
+
+}
